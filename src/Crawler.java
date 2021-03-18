1b08e3958e4a90e7c4386dd0b94cf3afacf3d40f
lduan <45192148+lduan97@users.noreply.github.com>
2021-03-17 22:25:38 -0500

    Add web crawling

---
+++ src/Crawler.java
@@ -0,0 +1,59 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashSet;
+
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import static org.jsoup.Jsoup.*;
+
+public class Crawler {
+	private static HashSet<String> urls = new HashSet<>();
+
+	public static void getURLs(String url) {
+		if (!urls.contains(url)) {
+			Document document;
+			try {
+				document = connect(url).timeout(5000).get();
+
+				Elements links = document.select("a[href^=\"https://www.cbc.ca/news\"]");
+				for (Element link : links) {
+					if (urls.size() < 500) {
+						urls.add(url);
+						getURLs(link.attr("abs:href"));
+					}
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static void getContent() {
+		try {
+			FileWriter link = new FileWriter("URLs.txt");
+			for (String url : urls) {
+				Document document;
+				document = connect(url).timeout(5000).maxBodySize(0).get();
+				String fileName = document.title().replace(" | CBC News", "").replaceAll("[^A-Za-z0-9\\s]", "")
+						+ ".txt";
+				File f = new File("..\\Web Search Engine\\Web Pages\\" + fileName);
+				f.getParentFile().mkdirs();
+				FileWriter wp = new FileWriter(f);
+				wp.write(document.body().text());
+				link.write(url + System.getProperty("line.separator"));
+				wp.close();
+			}
+			link.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		getURLs("https://www.cbc.ca/news");
+		getContent();
+	}
+}
